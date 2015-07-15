42cc695e99342a18b8d62ab45639e15c2b6d1e99
red24985 <gavinguthrie@icloud.com>
2015-07-15 14:13:50 +0100

    update

--- Assets/Script/Ball_Control.js
+++ Assets/Script/Ball_Control.js
@@ -3,7 +3,16 @@
 var rotationSpeed = 100;
 var jumpHeight = 8;
 
-private var isFalling = false;
+var Hit01 : AudioClip;
+var Hit02 : AudioClip;
+var Hit03 : AudioClip;
+
+var distToGround : float;
+
+function Start () {
+	// Getting the distance from the center to the ground.
+	distToGround = collider.bounds.extents.y;
+}
 
 function Update ()
 {
@@ -12,14 +21,29 @@ function Update ()
 	rotation *= Time.deltaTime;
 	rigidbody.AddRelativeTorque (Vector3.back * rotation);
 	
-	if (Input.GetKeyDown(KeyCode.Space) && isFalling == false)
+	if (Input.GetKeyDown(KeyCode.Space) && IsGrounded ())
 	{
 		rigidbody.velocity.y = jumpHeight;
 	}
-	isFalling = true;
 }
 
-function OnCollisionStay ()
-{
-	isFalling = false;
+function IsGrounded () : boolean { //Check if we are on the ground. Return true if we are else return null.
+	return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1);
+}
+
+function OnCollisionEnter () {
+	var theHit = Random.Range(0, 3);
+	if (theHit == 0)
+	{
+		audio.clip = Hit01;
+	}
+	else if (theHit == 1)
+	{
+		audio.clip = Hit02;
+	}
+	else {
+		audio.clip = Hit03;
+	}
+	audio.pitch = Random.Range (0.9,1.1);
+	audio.Play();
 }
